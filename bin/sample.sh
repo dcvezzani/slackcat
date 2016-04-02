23c592a3b6a375d45aab5dc7ea8f8e223da55156
David Vezzani <david_vezzani@yahoo.com>
2016-04-01 17:50:03 -0700

    sample now places valid code in clipboard

--- bin/sample.sh
+++ bin/sample.sh
@@ -3,7 +3,7 @@
 pat=":"
 if [[ "$TSDATE" =~ [$pat] ]]; then
   target_date="${TSDATE#*:}"
-  date_options='--tsrange $target_date:$target_date'
+  date_options='--tsrange $TSDATE'
 else
   if [ "$TSDATE" == '' ]; then
     target_date=$(date +'%Y-%m-%d')
@@ -15,5 +15,5 @@ fi
 
 echo "Copying sample for $target_date using '$date_options'"
 
-slackcat_cmd=$(printf "%q" "REAL_TAB=\$(echo -e \"\\\\t\"); target_date=\$target_date; SLACK_TOKEN=\$(cat ~/.slackcat) slackcat --timesheet $date_options; echo -e \$(cat time_sheet_report.txt) | sed 'x;G;1!h;s/\\\\n//g;\$!d' | jq -r '.report | to_entries | map(select(.key != \"hours\") | .value.entries | map(\"\(.ts) \(.diff) \(.permalink) \(.text)\") | join(\"\\\\n\")) | join(\"\\\\n\\\\n\")' | sed '/^\([[:digit:]]\{4\}\)-\([[:digit:]]\{2\}\)-\([[:digit:]]\{2\}\) \([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\) [^[:space:]]\{1,\} \([\.[:digit:]]\{1,\}\) \([^[:space:]]\{1,\}\) \(.*\)/ {; s/^\([[:digit:]]\{4\}\)-\([[:digit:]]\{2\}\)-\([[:digit:]]\{2\}\) \([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\) [^[:space:]]\{1,\} \([\.[:digit:]]\{1,\}\) \([^[:space:]]\{1,\}\) \(.*\)/\\\\1'\$REAL_TAB'\\\\2'\$REAL_TAB'\\\\3'\$REAL_TAB'\\\\4'\$REAL_TAB'\\\\5'\$REAL_TAB'\\\\6'\$REAL_TAB'\\\\7'\$REAL_TAB\$REAL_TAB\$REAL_TAB\$REAL_TAB\$REAL_TAB\$REAL_TAB\$REAL_TAB\$REAL_TAB'\\\\9'\$REAL_TAB'\\\\8/; }' > time_sheet_report-\$target_date.txt; cat time_sheet_report-\$target_date.txt | pbcopy; echo \"hours today: \$(cat time_sheet_report.txt | jq '(.report.\"'\$target_date'\".hours)')\"; echo \"hours for the period: \$(cat time_sheet_report.txt | jq '(.report.hours)')\"; echo \"mvim time_sheet_report-\$target_date.txt\""); echo -e "$slackcat_cmd" | sed -E 's/\\(.)/\1/g' | pbcopy
+slackcat_cmd=$(printf "%q" "REAL_TAB=\$(echo -e \"\\\\t\"); target_date=$target_date; SLACK_TOKEN=\$(cat ~/.slackcat) slackcat --timesheet $date_options; echo -e \$(cat time_sheet_report.txt) | sed 'x;G;1!h;s/\\\\n//g;\$!d' | jq -r '.report | to_entries | map(select(.key != \"hours\") | .value.entries | map(\"\(.ts) \(.diff) \(.permalink) \(.text)\") | join(\"\\\\n\")) | join(\"\\\\n\\\\n\")' | sed '/^\([[:digit:]]\{4\}\)-\([[:digit:]]\{2\}\)-\([[:digit:]]\{2\}\) \([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\) [^[:space:]]\{1,\} \([\.[:digit:]]\{1,\}\) \([^[:space:]]\{1,\}\) \(.*\)/ {; s/^\([[:digit:]]\{4\}\)-\([[:digit:]]\{2\}\)-\([[:digit:]]\{2\}\) \([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\) [^[:space:]]\{1,\} \([\.[:digit:]]\{1,\}\) \([^[:space:]]\{1,\}\) \(.*\)/\\\\1'\$REAL_TAB'\\\\2'\$REAL_TAB'\\\\3'\$REAL_TAB'\\\\4'\$REAL_TAB'\\\\5'\$REAL_TAB'\\\\6'\$REAL_TAB'\\\\7'\$REAL_TAB\$REAL_TAB\$REAL_TAB\$REAL_TAB\$REAL_TAB\$REAL_TAB\$REAL_TAB\$REAL_TAB'\\\\9'\$REAL_TAB'\\\\8/; }' > time_sheet_report-\$target_date.txt; cat time_sheet_report-\$target_date.txt | pbcopy; echo \"hours today: \$(cat time_sheet_report.txt | jq '(.report.\"'\$target_date'\".hours)')\"; echo \"hours for the period: \$(cat time_sheet_report.txt | jq '(.report.hours)')\"; echo \"mvim time_sheet_report-\$target_date.txt\""); echo -e "$slackcat_cmd" | sed -E 's/\\(.)/\1/g' | pbcopy
 
